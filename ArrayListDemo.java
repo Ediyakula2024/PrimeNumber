088ace050cfa791c2a4d0bf6c731cd96331423ad
Ediyakula2024 <ediyakulavaraprasad17112024@gmail.com>
2025-09-05 20:18:15 +0530

    ArrayListDemo

---
+++ ArrayListDemo.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class ArrayListDemo{
+    public static void main(String[] args) {
+        List<Object> s=new ArrayList<>();
+        s.add("Ediyakula");
+        s.add("varaprasad");
+        s.add(25);
+        s.add(3.14);
+        System.out.println(s);
+    }
+}
