02972cd83e7119607ab9a6bb50116014551ed2b8
Ediyakula2024 <ediyakulavaraprasad17112024@gmail.com>
2025-09-07 13:09:02 +0530

    Find the Targeted Index

---
+++ TargetSum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class TargetSum{
+    public static void main(String[] args){
+    int a[]={2,11,7,15};
+    int k=9;
+    List<Integer> s=new ArrayList<>();
+    for(int i=0;i<=a.length-1;i++){
+        for(int j=i+1;j<=a.length-1;j++){
+            if(a[i]+a[j]==k){
+                s.add(i);
+                s.add(j);
+            }
+        }
+    }
+    System.out.println(s);
+    }
+}
